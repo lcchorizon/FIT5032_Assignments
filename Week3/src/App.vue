9d970b0778b6edd30026cdd7d467262b7afb2ee2
hlii0325 <hlii0325@student.monash.edu>
2026-08-21 14:37:51 +1000

    git push origin main

---
+++ Week3/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+// import JSONLab from './components/JSON.vue'
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+</script>
+
+<template>
+  <!-- <JSONLab /> -->
+  <LibraryRegistrationForm />
+</template>
